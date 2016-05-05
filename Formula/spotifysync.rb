ca655ecfff5ee96d308f2fb0a78a525065857d78
Ze'ev Klapow <zklapow@hubspot.com>
2016-05-04 20:28:08 -0400

    install key and config files

--- Formula/spotifysync.rb
+++ Formula/spotifysync.rb
@@ -1,7 +1,8 @@
-class SpotifySync < Formula
+class Spotifysync < Formula
   desc "Synced spotify playing"
-  url "https://github.com/zklapow/SpotifySync/releases/download/v0.0.1/dist.zip"
-  sha256 "97b165652d8b885ff96b6985f46915760a5255c65a330b2b6219e3111e995b68"
+  url "https://github.com/zklapow/SpotifySync/releases/download/0.0.4/dist.zip"
+  sha256 "5615ae196e08019741f5acfa5a6a3dd5b09456b3c544adc4c7922d437b7b39d7"
+  version "0.0.4"
 
   # depends_on "cmake" => :build
   depends_on "libspotify"
@@ -10,6 +11,13 @@ class SpotifySync < Formula
 
   def install
     system "ls"
+    bin.install "release/spotifysyncclient", "release/spotifysync"
+    mkdir_p etc/"spotifysync"
+    FileUtils.install "release/config.toml", etc/"spotifysync/"
+    FileUtils.install "release/spotify_appkey.key", etc/"spotifysync/"
+    if !File.exist?(HOMEBREW_PREFIX/"opt/libspotify/lib/libspotify")
+      ln_s HOMEBREW_PREFIX/"opt/libspotify/lib/libspotify.dylib", HOMEBREW_PREFIX/"opt/libspotify/lib/libspotify"
+    end
     # system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
     # system "cmake", ".", *std_cmake_args
     # system "make", "install"

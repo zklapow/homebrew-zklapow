4fc18c04652500907c4f003397be5d4791caa0c0
Ze'ev Klapow <zklapow@hubspot.com>
2016-05-09 09:49:31 -0400

    Spotifysync: v0.0.6

--- Formula/spotifysync.rb
+++ Formula/spotifysync.rb
@@ -1,8 +1,8 @@
 class Spotifysync < Formula
   desc "Synced spotify playing"
-  url "https://github.com/zklapow/SpotifySync/releases/download/v0.0.5/dist.zip"
-  sha256 "c92f268e7e85e5fa0f3d5963671959aa02582d9592d42f8a3b40be394cb7d3c8"
-  version "0.0.5"
+  url "https://github.com/zklapow/SpotifySync/releases/download/v0.0.6/dist.zip"
+  sha256 "9ade5956db0f2f35599559531dd59e775db7222da9920eb0687ab5bc7e4ce857"
+  version "0.0.6"
 
   # depends_on "cmake" => :build
   depends_on "libspotify"
